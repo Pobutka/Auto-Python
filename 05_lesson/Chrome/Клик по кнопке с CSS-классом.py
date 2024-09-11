6abd6dfabd5032116cf37aca2338e4c7177a57ce
Nikita <generalpobuta98@gmail.com>
2024-09-11 19:38:48 +0300

    HW5

--- 05_lesson/Chrome/Клик по кнопке с CSS-классом.py
+++ 05_lesson/Chrome/Клик по кнопке с CSS-классом.py
@@ -5,7 +5,7 @@ import time
 
 
 service = Service(
-    r'C:\Pobuta\Desktop\Selinium\chromedriver-win64')
+    r'C:\Users\adm\.cache\selenium\chromedriver\win64\128.0.6613.119\chromedriver.exe')
 
 
 driver = webdriver.Chrome(service=service)
@@ -17,12 +17,8 @@ try:
     button = driver.find_element(By.CLASS_NAME, 'btn-primary')
     button.click()
 
-    alert = driver.switch_to.alert
-    alert.accept()
-
     time.sleep(10)
 
 finally:
     driver.quit()
-
 
